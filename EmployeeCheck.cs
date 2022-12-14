b9f5ec8679e20012421aef2548d6e0266be3e425
firsttoken <monikasharma12330@gmail.com>
2022-12-14 14:59:33 +0530

    CheckAttendence

---
+++ EmployeeCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeCheck
+    {
+          public static void CheckAttendence()
+          {
+                int full_time = 1;
+ 
+                Random randomno = new Random();
+                int ECheck = randomno.Next(2);
+                Console.WriteLine(ECheck);
+                if (ECheck == full_time)
+                {
+                    Console.WriteLine("Employee is Present.");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is Absend.");
+                }
+              
+          }
+     }
+}
