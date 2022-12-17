6c87131d6d312c0cdbedc3d57b5951cbbe0e5c8e
firsttoken <monikasharma12330@gmail.com>
2022-12-17 19:55:14 +0530

    UC4_Switch_EmployeeCheck

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -8,10 +8,11 @@ namespace EmployeeWageProblem
 {
     public class EmployeeCheck
     {
+        public const int full_time = 1;
+        public const int part_time = 2;
         public static void CheckAttendence()
         {
-            int full_time = 1;
-            int part_time = 2;
+
             int per_Hour = 20;
 
             int emp_Hour = 0;
@@ -22,20 +23,19 @@ namespace EmployeeWageProblem
             Random randomno = new Random();
             int ECheck = randomno.Next(3);
             // Console.WriteLine("Today Employee Attendence Status is: "+ECheck);
-            if (ECheck == full_time)
+            switch (ECheck)
             {
-                //Console.WriteLine("Employee is Present.");
-                emp_Hour = 8;
-            }
-            else if (ECheck == part_time)
-            {
-                //Console.WriteLine("Employee is Present Part time.");
-                emp_Hour = 4;
-            }
-            else
-            {
-                //Console.WriteLine("Employee is Absent.");
-                emp_Hour = 0;
+                case full_time:
+                    emp_Hour = 8;
+                    break;
+
+                case part_time:
+                    emp_Hour = 4;
+                    break;
+
+                default:
+                    emp_Hour = 0;
+                    break;
             }
             emp_Wage = emp_Hour * per_Hour;
             Console.WriteLine("Today wage of Employee: " + emp_Wage + " Rs.");
